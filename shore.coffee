banksJeremy/shore

commit c17c1db8665c446bd68d82bdeded4df6c239ef84

Mostly style changes.

shore.coffee
@@ -103,7 +103,7 @@ for name, value of { # contents of module
 		precedence: 0
 		
 		eq: (other) ->
-			@type == other.type and @_eq other
+			@type is other.type and @_eq other
 		
 		canonize: (enough, excess) ->
 			enough = shore._significance enough
@@ -184,7 +184,7 @@ for name, value of { # contents of module
 		precedence: 10
 		constructor: (@value) ->
 		
-		_eq: (other) -> @value == other.value
+		_eq: (other) -> @value is other.value
 		neg: -> shore.number (- @value)
 		to_free_tex: -> String @value
 		to_free_string: -> String @value
@@ -199,11 +199,11 @@ for name, value of { # contents of module
 				else
 					@tex_value = @string_value
 		
-		_eq: (other) -> @value == other.value
+		_eq: (other) -> @value is other.value
 		to_free_tex: -> @tex_value
 		to_free_string: -> @string_value
 		to_js: ->
-			if @string_value != @tex_value
+			if @string_value isnt @tex_value
 				"S.identifier(\"#{@string_value}\", \"#{@tex_value}\")"
 			else
 				"S(\"#{@string_value}\")"
@@ -219,7 +219,7 @@ for name, value of { # contents of module
 		constructor: (@operands) ->
 		
 		_eq: (other) ->
-			if @operands.length != other.operands.length
+			if @operands.length is other.operands.length
 				return false
 			
 			for i in [0..@operands.length - 1]
@@ -262,7 +262,7 @@ for name, value of { # contents of module
 			   operands[0].type is "Number" and
 				 operands[1].type isnt "Number"
 				
-				(if operands[0].value != -1 then operands[0].to_tex @precedence else "-") +
+				(if operands[0].value is -1 then operands[0].to_tex @precedence else "-") +
 				(((operand.to_tex @precedence) for operand in operands.slice 1)
 				 .join @tex_symbol)
 			else
@@ -274,17 +274,17 @@ for name, value of { # contents of module
 			negative_exponents = []
 			
 			for term in @operands
-				if term.type == "Exponent"
+				if term.type is "Exponent"
 					exponent = term.exponent
 					
-					if exponent.type == "Number" and exponent.value < 0
+					if exponent.type is "Number" and exponent.value < 0
 						negative_exponents.push shore.exponent term.base, exponent.neg()
 					else
 						positive_exponents.push term
 				else
 					positive_exponents.push term
 			
-			positive_exponents ||= [shore 1]
+			positive_exponents or= [shore 1]
 			
 			# log (o.to_string() for o in @operands)
 			
@@ -306,13 +306,13 @@ for name, value of { # contents of module
 		_eq: (other) -> @base.eq(other.base) and @exponent.eq(other.exponent)
 		
 		to_free_tex: ->
-			if @exponent.type == "Number" and @exponent.value == 1
+			if @exponent.type is "Number" and @exponent.value is 1
 				@base.to_tex @precedence
 			else
 				"{#{@base.to_tex @precedence}}^{#{@exponent.to_tex()}}"
 		
 		to_free_string: ->
-			if @exponent.type == "Number" and @exponent.value == 1
+			if @exponent.type is "Number" and @exponent.value is 1
 				@base.to_tex @precedence
 			else
 				"#{@base.to_string @precedence}^#{@exponent.to_string()}"
@@ -401,9 +401,9 @@ for name, getter_of_canonizers of {
 	CANOperation: ->
 		CANOperation.__super__._get_canonizers.apply(this).concat [
 			canonization "minor", "single argument", ->
-				@operands[0] if @operands.length == 1
+				@operands[0] if @operands.length is 1
 			canonization "minor", "no arguments", ->
-				@get_nullary() if @operands.length == 0 and @get_nullary
+				@get_nullary() if @operands.length is 0 and @get_nullary
 		]
 	
 	Sum: ->

shore.js
@@ -314,7 +314,7 @@
       __extends(CANOperation, Value);
       CANOperation.prototype._eq = function(other) {
         var i;
-        if (this.operands.length !== other.operands.length) {
+        if (this.operands.length === other.operands.length) {
           return false;
         }
         for (i = 0; (0 <= this.operands.length - 1 ? i <= this.operands.length - 1 : i >= this.operands.length - 1); (0 <= this.operands.length - 1 ? i += 1 : i -= 1)) {
@@ -384,7 +384,7 @@
       Product.prototype._to_free_tex = function(operands) {
         var _i, _len, _ref2, _result, operand;
         "Without checking for negative powers.";
-        return operands.length > 1 && operands[0].type === "Number" && operands[1].type !== "Number" ? (operands[0].value !== -1 ? operands[0].to_tex(this.precedence) : "-") + ((function() {
+        return operands.length > 1 && operands[0].type === "Number" && operands[1].type !== "Number" ? (operands[0].value === -1 ? operands[0].to_tex(this.precedence) : "-") + ((function() {
           _result = []; _ref2 = operands.slice(1);
           for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
             operand = _ref2[_i];

index.html
@@ -1,12 +1,13 @@
 <!doctype html>
 <html lang=en>
 	<head>
-		<title>Shore Math Engine</title>
+		<title>Shore Math [Noun]</title>
 		<meta charset="utf-8">
 		
 		<link rel=stylesheet href=style.css>
 		
 		<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7">
+		<!-- this is required for MathJax to work properly in IE8+ -->
 		
 		<script src="dep/jquery-1.3.4.js"></script>
 		<script src="dep/jquery.address-1.3.js"></script>
@@ -20,7 +21,7 @@
 		<div class=centering>
 			<div class="content box">
 				<h1>
-					<a href="./">Shore <span>Math Engine</span></a>
+					<a href="./">Shore <span>Math [Noun]</span></a>
 				</h1>
 				
 				<form>

main.coffee
@@ -6,10 +6,8 @@ default_input = """
 	t_fall = t(d=0)
 """
 
-shore.__main_mj_ready = ->
-
 mathjax_src = "dep/mathjax-1.0.1/MathJax.js"
-
+shore.__main_on_mathjax_loaded = (->)
 mathjax_load = ->
 	# http://www.mathjax.org/docs/dynamic.html
 	# apparently creating script tags with jQuery had oddities...
@@ -22,15 +20,15 @@ mathjax_load = ->
 			jax: ["input/TeX", "output/HTML-CSS"],
 			extensions: ["tex2jax.js", "TeX/AMSmath.js", "TeX/AMSsymbols.js"],
 			tex2jax: {
-				inlineMath: [["\\(", "\\)"]],
-				displayMath: [["\\[", "\\]"]]
+				inlineMath: [["\\\\(", "\\\\)"]],
+				displayMath: [["\\\\[", "\\\\]"]]
 			},
 			messageStyle: "none"
 		});
 		
 		MathJax.Hub.Startup.onload();
 		MathJax.Hub.Register.StartupHook("End", function() {
-			shore.__main_mj_ready()
+			shore.__main_on_mathjax_loaded()
 		});
 	"""
 	
@@ -85,7 +83,7 @@ occurences = (string, target_character) ->
 	
 	result = 0
 	for character in string
-		if character == target_character
+		if character is target_character
 			result += 1
 	result
 
@@ -100,8 +98,10 @@ escape_html = (raw) ->
 texscapeify = (value) ->
 	((escape_html value.to_tex()).replace /=/, "&=")
 
-process_math = (input, output) ->
-	output.show 300
+process_math = (input, output_element) ->
+	"Parses an input string then display and format the input, steps and result
+	in a given element."
+	
 	parsed = []
 	try
 		for line in input.split /\n/
@@ -114,106 +114,116 @@ process_math = (input, output) ->
 	catch e
 		throw e if not /^(Parse|Lexical) error/.test e.message # lack of exception type...
 		
-		output.empty()
-		output.append (($ "<pre>").css whiteSpace: "pre-line").
-			text e.message.replace "on line 1", "in \"#{expression}\""
-		output.show()
+		output_element.empty()
+		output_element.append (($ "<pre>").css whiteSpace: "pre-line")
+			.text e.message.replace "on line 1", "in \"#{expression}\""
+		output_element.show()
 		return
 	
-	output_parts = []
+	output_parts = [];
+	out = (s) -> output_parts.push s
 	
 	if MathJax?.isReady
-		output_parts.push "<h3><span class=tex2jax_ignore>Input</span></h3>"
-		output_parts.push "<div>\\begin{align}"
+		out "<h3 id=output_input>Input</h3>"
+		out "<div>\\begin{align}"
 		
 		for line in parsed
 			for expression in line
-				output_parts.push texscapeify expression
-				output_parts.push " & "
-			output_parts.push " \\\\\n<br>"
+				out texscapeify expression
+				out " & "
+			out " \\\\\n<br>"
 		
-		output_parts.push "\\end{align}</div>"
+		out "\\end{align}</div>"
 		
-		output_parts.push "<h3><span class=tex2jax_ignore>Steps</span></h3>"
-		output_parts.push "<div>\\begin{align}"
-		output_parts.push "\\end{align}</div>"
+		out "<h3 id=output_steps>Steps</h3>"
+		out "<div>\\begin{align}"
+		out "\\end{align}</div>"
 		
-		output_parts.push "<h3><span class=tex2jax_ignore>Results</span></h3>"
-		output_parts.push "<div>\\begin{align}"
+		out "<h3 id=output_results>Results</h3>"
+		out "<div>\\begin{align}"
 		
 		for line in parsed
 			for expression in line
-				output_parts.push texscapeify expression.canonize()
-				output_parts.push " & "
-			output_parts.push " \\\\\n<br>"
+				out texscapeify expression.canonize()
+				out " & "
+			out " \\\\\n<br>"
 		
-		output_parts.push "\\end{align}</div>"
+		out "\\end{align}</div>"
 		output.html output_parts.join ""
 		
 		MathJax.Hub.Queue ["Typeset", MathJax.Hub, (output.get 0) ]
-	else
-		output_parts.push "<h3>Input</h3>"
-		output_parts.push "<pre>"
+	else # plain text output
+		out "<h3 id=output_input>Input</h3>"
+		out "<pre>"
 		
 		for line in parsed
 			for expression in line
-				output_parts.push escape_html expression.to_string()
-				output_parts.push "\t"
-			output_parts.push "\n"
+				out escape_html expression.to_string()
+				out "\t"
+			out "\n"
 		
-		output_parts.push "</pre>"
+		out "</pre>"
 		
-		output_parts.push "<h3>Steps</h3>"
-		output_parts.push "<pre>"
-		output_parts.push "</pre>"
+		out "<h3 id=output_steps>Steps</h3>"
+		out "<pre>"
+		out "</pre>"
 		
-		output_parts.push "<h3>Results</h3>"
-		output_parts.push "<pre>"
+		out "<h3 id=output_results>Results</h3>"
+		out "<pre>"
 		
 		for line in parsed
 			for expression in line
-				output_parts.push escape_html expression.canonize().to_string()
-				output_parts.push "\t"
-			output_parts.push "\n"
-	
-		output_parts.push "</pre>"
+				out escape_html expression.canonize().to_string()
+				out "\t"
+			out "\n"
 		
-		output.html output_parts.join ""
+		out "</pre>"
+		
+		output_element.html output_parts.join ""
 	
-	($ "h3").css cursor: "pointer"
-	($ "h3").hover (-> ($ this).css backgroundColor: "rgba(0,0,0,.1)"),
-	               (-> ($ this).css backgroundColor: "transparent")
-	($ "h3").toggle (-> ($ this).next().show 300), (-> ($ this).next().hide 300)
+	($ "h3")
+		.css(cursor: "pointer")
+		.hover (-> ($ this).css backgroundColor: "rgba(0,0,0,.1)"),
+		       (-> ($ this).css backgroundColor: "transparent")
+		.toggle (-> ($ this).next().show 300),
+		        (-> ($ this).next().hide 300)
 	($ "h3 + div").hide()
-	(($ "h3").eq 0).click()
-	(($ "h3").eq 2).click()
+	($ "h3#output_input").click()
+	($ "h3#output_results").click()
+	
+	output.show 300
 
+$.fn.select_all = ->
+	"Sets the user's selection in an input/textarea to the complete contents."
+	
+	for element in this
+		element.selectionStart = 0
+		element.selectionEnd = element.value.length
+	
+	this
 
 $ main = ->
 	"Make it all start working when the DOM's ready."
 	
 	"Since it's not strictly necessary we don't load MathJax until after all
 	of the required scripts."
-	mathjax_load()
 	
+	mathjax_load()
 	qs = get_qs()
 	
 	input_box = $ "#input"
 	result_box = $ "#results"
 	form = $ "form"
 	
-	input_box.focus()
-	
 	em_pixels = ems_per_pixel_in $ "body"
 	
 	input_box.keypress (event) ->
-		if event.which == 13 or event.which == 10
+		if event.which is 13 or event.which is 10
 			if event.shiftKey
 				($ "form").submit()
-				false
+				false # suppress newline
 			else
 				scale_textarea input_box, +1
-				# where it most needs to be snappy
 	
 	input_box.keyup (event) -> scale_textarea input_box, em_pixels
 	scale_textarea input_box, em_pixels
@@ -223,22 +233,26 @@ $ main = ->
 		process_math input, result_box
 		window.location.hash = "i=#{encode input}"
 		
-		false # prevent form from being submitted normally
+		false # suppress normal form submission
 	
-	provided_input = qs.i or if (location.hash.slice 0, 3) is "#i="
+	provided_input = if (location.hash.slice 0, 3) is "#i="
 		decode location.hash.slice 3
+	else
+		qs.i
 	
-	input = provided_input || default_input
+	input = provided_input or default_input
 	input_box.val input
 	scale_textarea input_box, em_pixels
 	
-	(input_box.get 0).selectionStart = 0
-	(input_box.get 0).selectionEnd = input_box.val().length
+	input_box
+		.select_all()
+		.focus()
 	
 	if provided_input
-		"We give MathJax two seconds to load, then fall back to plain text."
+		"We give MathJax two seconds to load, then process into plain text if
+		necessary."
 		
-		if mj_wait
+		if mj_wait and not MathJax?.isReady
 			processed = false
 			
 			process_once = ->
@@ -246,8 +260,7 @@ $ main = ->
 				processed = true
 				process_math input, result_box if provided_input
 			
-			shore.__main_mj_ready = ->
-				process_once()
+			shore.__main_on_mathjax_loaded = process_once
 			setTimeout process_once, mj_wait
 		else
 			process_math input, result_box if provided_input

main.js
@@ -1,14 +1,14 @@
 (function() {
   var decode, default_input, ems_per_pixel_in, encode, escape_html, get_qs, main, mathjax_load, mathjax_src, mj_wait, occurences, process_math, scale_textarea, texscapeify;
   default_input = "A = -g_gemini; g_gemini = 0.00015\nv = A ~ t + v_0; v_0 = 0\nd = v ~ t + d_0; d_0 = 1\nt_fall = t(d=0)";
-  shore.__main_mj_ready = function() {};
   mathjax_src = "dep/mathjax-1.0.1/MathJax.js";
+  shore.__main_on_mathjax_loaded = (function() {});
   mathjax_load = function() {
     var _ref, config, script;
     script = document.createElement("script");
     script.type = "text/javascript";
     script.src = mathjax_src;
-    config = "MathJax.Hub.Config({\n	jax: [\"input/TeX\", \"output/HTML-CSS\"],\n	extensions: [\"tex2jax.js\", \"TeX/AMSmath.js\", \"TeX/AMSsymbols.js\"],\n	tex2jax: {\n		inlineMath: [[\"\\(\", \"\\)\"]],\n		displayMath: [[\"\\[\", \"\\]\"]]\n	},\n	messageStyle: \"none\"\n});\n\nMathJax.Hub.Startup.onload();\nMathJax.Hub.Register.StartupHook(\"End\", function() {\n	shore.__main_mj_ready()\n});";
+    config = "MathJax.Hub.Config({\n	jax: [\"input/TeX\", \"output/HTML-CSS\"],\n	extensions: [\"tex2jax.js\", \"TeX/AMSmath.js\", \"TeX/AMSsymbols.js\"],\n	tex2jax: {\n		inlineMath: [[\"\\\\(\", \"\\\\)\"]],\n		displayMath: [[\"\\\\[\", \"\\\\]\"]]\n	},\n	messageStyle: \"none\"\n});\n\nMathJax.Hub.Startup.onload();\nMathJax.Hub.Register.StartupHook(\"End\", function() {\n	shore.__main_on_mathjax_loaded()\n});";
     if (typeof (_ref = window.opera) !== "undefined" && _ref !== null) {
       script.innerHTML = config;
     } else {
@@ -73,9 +73,10 @@ processing it to plain-text output.";
   texscapeify = function(value) {
     return (escape_html(value.to_tex())).replace(/=/, "&=");
   };
-  process_math = function(input, output) {
-    var _i, _j, _len, _len2, _ref, _ref2, expression, line, output_parts, parsed, parsed_line;
-    output.show(300);
+  process_math = function(input, output_element) {
+    var _i, _j, _len, _len2, _ref, _ref2, expression, line, out, output_parts, parsed, parsed_line;
+    "Parses an input string then display and format the input, steps and result\
+	in a given element.";
     parsed = [];
     try {
       _ref = input.split(/\n/);
@@ -97,86 +98,88 @@ processing it to plain-text output.";
       if (!/^(Parse|Lexical) error/.test(e.message)) {
         throw e;
       }
-      output.empty();
-      output.append((($("<pre>")).css({
+      output_element.empty();
+      output_element.append((($("<pre>")).css({
         whiteSpace: "pre-line"
       })).text(e.message.replace("on line 1", "in \"" + (expression) + "\"")));
-      output.show();
+      output_element.show();
       return null;
     }
     output_parts = [];
+    out = function(s) {
+      return output_parts.push(s);
+    };
     if ((typeof MathJax === "undefined" || MathJax === null) ? undefined : MathJax.isReady) {
-      output_parts.push("<h3><span class=tex2jax_ignore>Input</span></h3>");
-      output_parts.push("<div>\\begin{align}");
+      out("<h3 id=output_input>Input</h3>");
+      out("<div>\\begin{align}");
       _ref = parsed;
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         line = _ref[_i];
         _ref2 = line;
         for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
           expression = _ref2[_j];
-          output_parts.push(texscapeify(expression));
-          output_parts.push(" & ");
+          out(texscapeify(expression));
+          out(" & ");
         }
-        output_parts.push(" \\\\\n<br>");
+        out(" \\\\\n<br>");
       }
-      output_parts.push("\\end{align}</div>");
-      output_parts.push("<h3><span class=tex2jax_ignore>Steps</span></h3>");
-      output_parts.push("<div>\\begin{align}");
-      output_parts.push("\\end{align}</div>");
-      output_parts.push("<h3><span class=tex2jax_ignore>Results</span></h3>");
-      output_parts.push("<div>\\begin{align}");
+      out("\\end{align}</div>");
+      out("<h3 id=output_steps>Steps</h3>");
+      out("<div>\\begin{align}");
+      out("\\end{align}</div>");
+      out("<h3 id=output_results>Results</h3>");
+      out("<div>\\begin{align}");
       _ref = parsed;
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         line = _ref[_i];
         _ref2 = line;
         for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
           expression = _ref2[_j];
-          output_parts.push(texscapeify(expression.canonize()));
-          output_parts.push(" & ");
+          out(texscapeify(expression.canonize()));
+          out(" & ");
         }
-        output_parts.push(" \\\\\n<br>");
+        out(" \\\\\n<br>");
       }
-      output_parts.push("\\end{align}</div>");
+      out("\\end{align}</div>");
       output.html(output_parts.join(""));
       MathJax.Hub.Queue(["Typeset", MathJax.Hub, (output.get(0))]);
     } else {
-      output_parts.push("<h3>Input</h3>");
-      output_parts.push("<pre>");
+      out("<h3 id=output_input>Input</h3>");
+      out("<pre>");
       _ref = parsed;
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         line = _ref[_i];
         _ref2 = line;
         for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
           expression = _ref2[_j];
-          output_parts.push(escape_html(expression.to_string()));
-          output_parts.push("\t");
+          out(escape_html(expression.to_string()));
+          out("\t");
         }
-        output_parts.push("\n");
+        out("\n");
       }
-      output_parts.push("</pre>");
-      output_parts.push("<h3>Steps</h3>");
-      output_parts.push("<pre>");
-      output_parts.push("</pre>");
-      output_parts.push("<h3>Results</h3>");
-      output_parts.push("<pre>");
+      out("</pre>");
+      out("<h3 id=output_steps>Steps</h3>");
+      out("<pre>");
+      out("</pre>");
+      out("<h3 id=output_results>Results</h3>");
+      out("<pre>");
       _ref = parsed;
       for (_i = 0, _len = _ref.length; _i < _len; _i++) {
         line = _ref[_i];
         _ref2 = line;
         for (_j = 0, _len2 = _ref2.length; _j < _len2; _j++) {
           expression = _ref2[_j];
-          output_parts.push(escape_html(expression.canonize().to_string()));
-          output_parts.push("\t");
+          out(escape_html(expression.canonize().to_string()));
+          out("\t");
         }
-        output_parts.push("\n");
+        out("\n");
       }
-      output_parts.push("</pre>");
-      output.html(output_parts.join(""));
+      out("</pre>");
+      output_element.html(output_parts.join(""));
     }
     ($("h3")).css({
       cursor: "pointer"
-    });
-    ($("h3")).hover(function() {
+    }).hover(function() {
       return ($(this)).css({
         backgroundColor: "rgba(0,0,0,.1)"
       });
@@ -184,18 +187,29 @@ processing it to plain-text output.";
       return ($(this)).css({
         backgroundColor: "transparent"
       });
-    });
-    ($("h3")).toggle(function() {
+    }).toggle(function() {
       return ($(this)).next().show(300);
     }, function() {
       return ($(this)).next().hide(300);
     });
     ($("h3 + div")).hide();
-    (($("h3")).eq(0)).click();
-    return (($("h3")).eq(2)).click();
+    ($("h3#output_input")).click();
+    ($("h3#output_results")).click();
+    return output.show(300);
+  };
+  $.fn.select_all = function() {
+    var _i, _len, _ref, element;
+    "Sets the user's selection in an input/textarea to the complete contents.";
+    _ref = this;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      element = _ref[_i];
+      element.selectionStart = 0;
+      element.selectionEnd = element.value.length;
+    }
+    return this;
   };
   $(main = function() {
-    var em_pixels, form, input, input_box, process_once, processed, provided_input, qs, result_box, window;
+    var em_pixels, form, input, input_box, process_once, processed, provided_input, qs, result_box;
     "Make it all start working when the DOM's ready.";
     "Since it's not strictly necessary we don't load MathJax until after all\
 	of the required scripts.";
@@ -204,7 +218,6 @@ processing it to plain-text output.";
     input_box = $("#input");
     result_box = $("#results");
     form = $("form");
-    input_box.focus();
     em_pixels = ems_per_pixel_in($("body"));
     input_box.keypress(function(event) {
       if (event.which === 13 || event.which === 10) {
@@ -220,22 +233,22 @@ processing it to plain-text output.";
       return scale_textarea(input_box, em_pixels);
     });
     scale_textarea(input_box, em_pixels);
-    form.submit(window = function() {
+    form.submit(function() {
       var input;
       input = ($("#input")).val();
       process_math(input, result_box);
       window.location.hash = ("i=" + (encode(input)));
       return false;
     });
-    provided_input = qs.i || ((location.hash.slice(0, 3)) === "#i=" ? decode(location.hash.slice(3)) : null);
+    provided_input = (location.hash.slice(0, 3)) === "#i=" ? decode(location.hash.slice(3)) : qs.i;
     input = provided_input || default_input;
     input_box.val(input);
     scale_textarea(input_box, em_pixels);
-    (input_box.get(0)).selectionStart = 0;
-    (input_box.get(0)).selectionEnd = input_box.val().length;
+    input_box.select_all().focus();
     if (provided_input) {
-      "We give MathJax two seconds to load, then fall back to plain text.";
-      if (mj_wait) {
+      "We give MathJax two seconds to load, then process into plain text if\
+		necessary.";
+      if (mj_wait && !((typeof MathJax === "undefined" || MathJax === null) ? undefined : MathJax.isReady)) {
         processed = false;
         process_once = function() {
           if (processed) {
@@ -246,9 +259,7 @@ processing it to plain-text output.";
             return process_math(input, result_box);
           }
         };
-        shore.__main_mj_ready = function() {
-          return process_once();
-        };
+        shore.__main_on_mathjax_loaded = process_once;
         return setTimeout(process_once, mj_wait);
       } else {
         if (provided_input) {

commit ab21888d78e6cbe480eea9e70998177b7ccfdf2a

Fixed crash, was introduced by c17c1db.

shore.coffee
@@ -216,7 +216,7 @@ for name, value of { # contents of module
 		constructor: (@operands) ->
 		
 		_eq: (other) ->
-			if @operands.length is other.operands.length
+			if @operands.length isnt other.operands.length
 				return false
 			
 			for i in [0..@operands.length - 1]
@@ -259,7 +259,7 @@ for name, value of { # contents of module
 			   operands[0].type is "Number" and
 				 operands[1].type isnt "Number"
 				
-				(if operands[0].value is -1 then operands[0].to_tex @precedence else "-") +
+				(if operands[0].value isnt -1 then operands[0].to_tex @precedence else "-") +
 				(((operand.to_tex @precedence) for operand in operands.slice 1)
 				 .join @tex_symbol)
 			else

shore.js
@@ -308,7 +308,7 @@
       __extends(CANOperation, Value);
       CANOperation.prototype._eq = function(other) {
         var i;
-        if (this.operands.length === other.operands.length) {
+        if (this.operands.length !== other.operands.length) {
           return false;
         }
         for (i = 0; (0 <= this.operands.length - 1 ? i <= this.operands.length - 1 : i >= this.operands.length - 1); (0 <= this.operands.length - 1 ? i += 1 : i -= 1)) {
@@ -378,7 +378,7 @@
       Product.prototype._to_free_tex = function(operands) {
         var _i, _len, _ref2, _result, operand;
         "Without checking for negative powers.";
-        return operands.length > 1 && operands[0].type === "Number" && operands[1].type !== "Number" ? (operands[0].value === -1 ? operands[0].to_tex(this.precedence) : "-") + ((function() {
+        return operands.length > 1 && operands[0].type === "Number" && operands[1].type !== "Number" ? (operands[0].value !== -1 ? operands[0].to_tex(this.precedence) : "-") + ((function() {
           _result = []; _ref2 = operands.slice(1);
           for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
             operand = _ref2[_i];